aaee5ba35ded04df16450f5b41f8bf7e361ce418
jlkobane <jaysonkobane88@gmail.com>
2022-10-06 09:13:26 -0700

    Spellin

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home(props) {
 				<div className='box'>
 					<h1 className='welcome'>Welcome to the Marvel Wiki</h1>
 					<p className='info'>
-						Learn about your favorite Marvel characters and comics! All at the click of a button. So come join us in the fight againts
+						Learn about your favorite Marvel characters and comics! All at the click of a button. So come join us in the fight against
 						evil.
 					</p>
 				</div>
